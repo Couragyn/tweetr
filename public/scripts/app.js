d21ce508c38b26fc14d3703dfa5084b230fd22e2
Couragyn Chretien <couragyn@gmail.com>
2017-03-22 01:26:06 +0000

    Creating the tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,11 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  $('article').mouseover(function() {
+    $(this).find('header').removeClass("opaque");
+  });
+  $('article').mouseleave(function() {
+    $(this).find('header').addClass("opaque");
+  });
+});
